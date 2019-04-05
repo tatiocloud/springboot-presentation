9ab764246da6043c625a54e1e9575c0c44ac22aa
tatiocloud <scala4spark@gmail.com>
2019-04-05 09:07:58 +0100

    adding advanced jpa module

---
+++ advanced-jpa/src/test/java/com/tatiocloud/jpa/advanced/AdvancedJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tatiocloud.jpa.advanced;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvancedJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
